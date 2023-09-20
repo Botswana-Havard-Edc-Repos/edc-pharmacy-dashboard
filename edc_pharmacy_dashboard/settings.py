e7c5a2c4cf80307f75e5a18c7cace69f29ea9521
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-20 08:13:03 +0200

    Update settings.py

--- edc_pharmacy_dashboard/settings.py
+++ edc_pharmacy_dashboard/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django_js_reverse',
     'tz_detect',
     'edc_base.apps.AppConfig',
